16607a55add5d78cdff96e411a991ef042771b16
RandyChenXinhao <chen.randy@outlook.com>
2018-03-06 09:21:36 +0800

    New SharePoint Extension

---
+++ src/extensions/aiibSpfxExt/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface IAiibSpfxExtApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'AiibSpfxExtApplicationCustomizerStrings' {
+  const strings: IAiibSpfxExtApplicationCustomizerStrings;
+  export = strings;
+}
